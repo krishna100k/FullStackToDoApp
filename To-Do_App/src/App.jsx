b0a7624014848de973d8870cc42e42e7bd5649f7
krishna100k <102644745+krishna100k@users.noreply.github.com>
2023-08-09 18:05:32 +0530

    Update App.jsx

--- To-Do_App/src/App.jsx
+++ To-Do_App/src/App.jsx
@@ -105,7 +105,7 @@ function App() {
       <h1 className="heading">Another New Todo App</h1>
       <div className="leftPart">
         <div className="quote-box">
-          <h1>{quote[randomNumber].text}</h1>
+          <h1>{quote[1].text}</h1>
         </div>
         <form className="form" action="POST">
           <div className="inputs">
